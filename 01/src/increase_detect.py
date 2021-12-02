b299dd5cdc8503a608cd0de6e2f7df012e828f60
jtbg <justin@bassett.green>
2021-12-02 01:21:22 -0600

    refactors sonar reading to reduce memory reads

--- 01/src/increase_detect.py
+++ 01/src/increase_detect.py
@@ -1,22 +1,22 @@
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('../config.ini')
 
 with open(config['ASSETS']['input_file'], 'r') as f:
     readings = f.readlines()
 
+readings = list(map(int, readings))
+
 count_readings = 0
 count_increased_depths = 0
-for depth in readings:
-    depth = int(depth)
+for i in range(1, len(readings)):
     try:
-        if depth > prev_depth:
+        if readings[i] > readings[i-1]:
             count_increased_depths += 1
-    except NameError:
-        print('no previous reading found')
+    except:
+        print(f'''error at {count_readings+1} evaluating {readings[i]} > {readings[i-1]}''')
     finally:
-        prev_depth = depth
         count_readings += 1
 
 print(f'''Evaluated {count_readings} sonar readings and found {count_increased_depths} instances of increased depth''')
