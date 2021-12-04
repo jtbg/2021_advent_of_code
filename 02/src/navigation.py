68155ce92f7d7682f811678383b2bead220c5579
Justin Bassett-Green <justin@bassett.green>
2021-12-04 05:53:37 -0600

    Merge pull request #4
    * updates day 02 README with second task
    
    * adds aim parameter to depth navigation

--- 02/src/navigation.py
+++ 02/src/navigation.py
@@ -7,6 +7,7 @@ class Vessel:
         self.name = 'The ' + ' '.join(x.capitalize() for x in generate(2))
         self.x_pos = 0
         self.depth = 0
+        self.aim = 0
         self.odometer = 0
 
     def move(self, direction, magnitude):
@@ -14,10 +15,11 @@ class Vessel:
         self.odometer += 1
         if direction == "forward":
             self.x_pos += magnitude
+            self.depth += magnitude * self.aim
         elif direction == "down":
-            self.depth += magnitude
+            self.aim += magnitude
         elif direction == "up":
-            self.depth -= magnitude
+            self.aim -= magnitude
         else:
             logging.error(f'''invalid movement {direction} {magnitude}''')
 
